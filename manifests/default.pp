f2345be53cc6caef1855dac1303641f6b8f89c10
Ivar Abrahamsen <ivar@flurdy.com>
2014-06-06 18:19:50 +0100

    Cassandra Install

--- manifests/default.pp
+++ manifests/default.pp
@@ -7,28 +7,23 @@ exec { "uk-mirror":
   path       => '/bin',
 }
 
-
-#exec { 'add-webupd8-key':
-# command => 'apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886',
-#  path => '/usr/bin/',
-#}
-
 apt::key { 'webupd8':
   key        => 'EEA14886',
   key_server => 'keyserver.ubuntu.com',
 }
 
-# exec { 'add-datastax-key':
-#   command => 'curl -L http://debian.datastax.com/debian/repo_key | sudo apt-key add -',
-#   path => '/usr/bin/',
-# }
+exec { 'add-datastax-key':
+  command => 'curl -L http://debian.datastax.com/debian/repo_key | sudo apt-key add -',
+  path => '/usr/bin/',
+  require => Package["curl"],
+}
 
-# apt::source { 'datastax':
-#   location          =>  'http://debian.datastax.com/community',
-#   repos             =>  'main',
-#   release           =>  'stable',
-#   include_src       =>  false,
-# }
+apt::source { 'datastax':
+  location          =>  'http://debian.datastax.com/community',
+  repos             =>  'main',
+  release           =>  'stable',
+  include_src       =>  false,
+}
 
 apt::source { 'webupd8team':
   location          =>  'http://ppa.launchpad.net/webupd8team/java/ubuntu',
@@ -36,14 +31,6 @@ apt::source { 'webupd8team':
   release           =>  'precise',
   include_src       =>  false,
 }
-
-# apt::ppa { 'ppa:webupd8team/java': }
-
-#package { "python-software-properties":
-#  ensure  => present,
-#  require => Exec["apt-get update"],
-#}
-
 
 exec { 'accept-java-license':
   command => '/bin/echo /usr/bin/debconf shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections;/bin/echo /usr/bin/debconf shared/accepted-oracle-license-v1-1 seen true | sudo /usr/bin/debconf-set-selections;',
@@ -60,10 +47,16 @@ package {
  "oracle-java6-installer":
     ensure  => present,
     require => [Package["curl"],Exec["uk-mirror","apt-get update","accept-java-license"],Apt::Source["webupd8team"]],
-    before => Package["oracle-java6-set-default"];
+    before => Package["oracle-java6-set-default","dsc12","cassandra"];
   "oracle-java6-set-default":
     ensure  => present;
-    # "add-datastax-key",
+  "cassandra":
+    ensure  => "1.2.15",
+    before => Package["dsc12"],
+    require => [Package["oracle-java6-installer"],Exec["uk-mirror","apt-get update","accept-java-license","add-datastax-key"],Apt::Source["datastax"]];
+  "dsc12":
+    ensure  => "1.2.15-1",
+    require => [Package["oracle-java6-installer","cassandra"],Exec["uk-mirror","apt-get update","accept-java-license","add-datastax-key"],Apt::Source["datastax"]];
 }
 
 #service { "apache2":
